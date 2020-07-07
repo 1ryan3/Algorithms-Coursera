4f9d9b1a3dccf96d302c894b14601815e23a0e49
Ryan <ryan.mwood@outlook.com>
2020-07-07 09:21:26 -0700

    Finished initial PointSet implementation

--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -4,43 +4,89 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class PointSET {
-    public PointSET() {
+    private SET<Point2D> pointSet;
 
+
+    public PointSET() {
+        pointSet = new SET<>();
     }                              // construct an empty set of points
 
     public boolean isEmpty() {
-
+        return pointSet.isEmpty();
     }                    // is the set empty?
 
     public int size() {
-
+        return pointSet.size();
     }                        // number of points in the set
 
     public void insert(Point2D p) {
-
+        if (p == null) throw new IllegalArgumentException();
+        pointSet.add(p);
     }             // add the point to the set (if it is not already in the set)
 
     public boolean contains(Point2D p) {
-
+        if (p == null) throw new IllegalArgumentException();
+        return pointSet.contains(p);
     }           // does the set contain point p?
 
     public void draw() {
-
+        for (Point2D p : pointSet) {
+            p.draw();
+        }
     }                        // draw all points to standard draw
 
     public Iterable<Point2D> range(RectHV rect) {
-
+        SET<Point2D> temp = new SET<>();
+        double x = 0;
+        double y = 0;
+        for (Point2D p2 : pointSet) {
+            x = p2.x();
+            y = p2.y();
+            if ((x <= rect.xmax() && x >= rect.xmin()) && (y <= rect.ymax() && y >= rect.ymin())) {
+                temp.add(p2);
+            }
+        }
+        return temp;
     }            // all points that are inside the rectangle (or on the boundary)
 
     public Point2D nearest(Point2D p) {
 
+        Point2D temp = null;
+        double min = Double.POSITIVE_INFINITY;
+        double dist;
+
+        for (Point2D p2 : pointSet) {
+            dist = p.distanceTo(p2);
+            if (dist < min) {
+                min = dist;
+                temp = p2;
+            }
+        }
+        return temp;
     }            // a nearest neighbor in the set to point p; null if the set is empty
 
     public static void main(String[] args) {
+        String filename = args[0];
+        In in = new In(filename);
+        PointSET testSet = new PointSET();
+        while (!in.isEmpty()) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            Point2D p = new Point2D(x, y);
+            testSet.insert(p);
+        }
+
+        StdDraw.clear();
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        testSet.draw();
 
     }                 // unit testing of the methods (optional)
 
